caaa91c0072329f9e58b3d9efb9db9b02914de9c
Kamalesh Babulal <kamalesh@linux.vnet.ibm.com>
2016-05-10 11:21:02 +0530

    elf-diff-copy: Add elf-open()/log_d macro
    Introducing elf_open() to open object files and
    log_d macro for debugging.
    
    Signed-off-by: Kamalesh Babulal <kamalesh@linux.vnet.ibm.com>

--- elf-diff-copy/elf-diff-copy.c
+++ elf-diff-copy/elf-diff-copy.c
@@ -1,5 +1,18 @@
-#include <stdlib.h>
 #include <argp.h>
+#include <gelf.h>
+#include <error.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <elfutils/elf-knowledge.h>
+
+#ifdef DEBUG
+#define log_d(format, ...) \
+        printf("" format, ##__VA_ARGS__);
+#else
+#define log_d(format, ...) { };
+#endif
 
 struct arguments {
 	char *args[2];
@@ -8,6 +21,7 @@ struct arguments {
 };
 
 struct arguments args;
+Elf *elf1, *elf2;
 
 error_t parse_opt(int key, char *arg, struct argp_state *state)
 {
@@ -36,11 +50,13 @@ error_t parse_opt(int key, char *arg, struct argp_state *state)
 
 	return 0;
 }
+
 static const struct argp_option options[] = {
 	{NULL, 'v', "file", 0, "original vmlinux"},
 	{NULL, 'o', "file", 0, "output file"},
 	{},
 };
+
 static struct argp argp = {
 	.options	= options,
 	.parser		= parse_opt,
@@ -48,9 +64,35 @@ static struct argp argp = {
 	.doc		= "Compare two kernel .o files and generate an object containing the changed and/or new functions.",
 };
 
+static Elf *elf_open(const char *name, int *fd)
+{
+	Elf *elf;
+
+	*fd = open(name, O_RDONLY);
+	if (*fd == -1)
+		error(1, errno, "open of '%s' failed", name);
+
+	elf = elf_begin(*fd, ELF_C_READ_MMAP, NULL);
+	if (!elf)
+		error(1, 0, "elf_begin failed for '%s': %s", name,
+		      elf_errmsg(-1));
+
+	log_d("%s %s\t\t[PASSED]\n", __FUNCTION__, name);
+	return elf;
+}
+
 int main(int argc, char *argv[])
 {
+	int fd1, fd2;
+
 	argp_parse(&argp, argc, argv, 0, NULL, &args);
+
+	/*
+	 * Open ELF object files.
+	 */
+	elf_version(EV_CURRENT);
+	elf1 = elf_open(args.args[0], &fd1);
+	elf2 = elf_open(args.args[1], &fd2);
 
 	return (0);
 }
